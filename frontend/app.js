6adebaa5e8af8e745c52d7d43191f67c6413cc0e
tokukanhi96 <58257354+tokukanhi96@users.noreply.github.com>
2023-11-04 12:32:08 -0700

    Sprint 1: Creating basic graph

---
+++ frontend/app.js
@@ -0,0 +1,4 @@
+function generateGraph() {
+    // Your code to generate and display the graph goes here
+    alert('Graph generation function called'); // You can replace this with your actual graph generation code
+}
